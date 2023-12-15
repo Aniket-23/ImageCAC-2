f693aca57fb74bd22e7eeb494d26947806696f04
Aniket-23 <117341962+Aniket-23@users.noreply.github.com>
2023-12-15 13:50:01 +0530

    finished building and integrating landing page

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/uploadImage')
+def upload_image():
+    return render_template('uploadImage.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
